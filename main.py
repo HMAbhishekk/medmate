43f4d9d04ec07fe52fe93ccd5b30472452566995
Abhi____hiremath <155541138+HMAbhishekk@users.noreply.github.com>
2025-04-19 16:22:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import pytesseract
+
+# Set the path to the tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 import streamlit as st
 from PIL import Image
 import pytesseract
